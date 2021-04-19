1530bc66425e091f0f548a4eb65d04b234e080fb
Sergei Svistelnikov <sergei.svistelnikov@outlook.com>
2021-04-18 18:31:21 -0700

    TC-005

--- cypress/integration/smoke/smokeSpec.js
+++ cypress/integration/smoke/smokeSpec.js
@@ -32,6 +32,64 @@ describe('SMOKE TESTS SUITE', () => {
           cy.get(sel.name).should('be.visible');
       });
 
+      it('TC-006 Gender label is present', () => {
+          cy.contains('form', exp.genderLabel).then( genderLabel => {
+              const label = genderLabel.find('[for="gender"]').text();
+              expect(label).to.equal(exp.genderLabel);
+          });
+
+      });
+
+      // it('TC-007.a Gender radio button 1 is present', () => {
+      //     let buttonHe = $$(sel.radioButtons)[0].isDisplayed();
+      //     expect(buttonHe).toEqual(true);
+      // });
+      //
+      // it('TC-007.b Gender radio button 2 is present', () => {
+      //     let buttonShe = $$(sel.radioButtons)[1].isDisplayed();
+      //     expect(buttonShe).toEqual(true);
+      // });
+      //
+      // it('TC-007.c Gender radio button 3 is present', () => {
+      //     let buttonIt = $$(sel.radioButtons)[2].isDisplayed();
+      //     expect(buttonIt).toEqual(true);
+      // });
+      //
+      // it('TC-008 Age field label is present', () => {
+      //     let label = $$(sel.requiredLabel)[2].isDisplayed();
+      //     expect(label).toEqual(true);
+      // });
+      //
+      // it('TC-009 Age field is present', () => {
+      //     let age = $(sel.age).isDisplayed();
+      //     expect(age).toEqual(true);
+      // });
+      //
+      // it('TC-010 Story type field label is present', () => {
+      //     let label = $$(sel.requiredLabel)[3].isDisplayed();
+      //     expect(label).toEqual(true);
+      // });
+      //
+      // it('TC-011 Story type dropdown is present', () => {
+      //     let dropdown = $(sel.storyType).isDisplayed();
+      //     expect(dropdown).toEqual(true);
+      // });
+      //
+      // it('TC-012 Image field label is present', () => {
+      //     let label = $$(sel.imageLabel)[4].isDisplayed();
+      //     expect(label).toEqual(true);
+      // });
+      //
+      // it('TC-013 Image field is present', () => {
+      //     let image = $(sel.image).isDisplayed();
+      //     expect(image).toEqual(true);
+      // });
+      //
+      // it('TC-014 Submit button is present', () => {
+      //     let submit = $(sel.submit).isDisplayed();
+      //     expect(submit).toEqual(true);
+      // });
+
   });
 
 });
