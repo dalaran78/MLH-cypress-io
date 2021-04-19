1a40915545bf16ddd5ca6044acde0d5ae07b772f
Sergei Svistelnikov <sergei.svistelnikov@outlook.com>
2021-04-19 09:47:58 -0700

    Smoke is done

--- cypress/integration/smoke/smokeSpec.js
+++ cypress/integration/smoke/smokeSpec.js
@@ -1,5 +1,4 @@
 import sel from '../../fixtures/selectors';
-import exp from '../../fixtures/expected.json';
 
 describe('SMOKE TESTS SUITE', () => {
 
@@ -10,7 +9,7 @@ describe('SMOKE TESTS SUITE', () => {
   describe('Elements exist:', () => {
 
       it('TC-001 Page Title is MLH trial', () => {
-          cy.title().should('include', 'MLH trial')
+          cy.title().should('include', 'MLH trial');
       });
 
       it('TC-002 Login form is present', () => {
@@ -38,7 +37,7 @@ describe('SMOKE TESTS SUITE', () => {
       });
 
       it('TC-007-2 Gender "she" radio button is present', () => {
-        cy.get('[type="radio"]').check('she');
+          cy.get('[type="radio"]').check('she');
       });
 
       it('TC-007-3 Gender "it" radio button is present', () => {
@@ -56,26 +55,22 @@ describe('SMOKE TESTS SUITE', () => {
       it('TC-010 Story type field label is present', () => {
           cy.get(sel.storyLabel).should('be.visible');
       });
-      //
-      // it('TC-011 Story type dropdown is present', () => {
-      //     let dropdown = $(sel.storyType).isDisplayed();
-      //     expect(dropdown).toEqual(true);
-      // });
-      //
-      // it('TC-012 Image field label is present', () => {
-      //     let label = $$(sel.imageLabel)[4].isDisplayed();
-      //     expect(label).toEqual(true);
-      // });
-      //
-      // it('TC-013 Image field is present', () => {
-      //     let image = $(sel.image).isDisplayed();
-      //     expect(image).toEqual(true);
-      // });
-      //
-      // it('TC-014 Submit button is present', () => {
-      //     let submit = $(sel.submit).isDisplayed();
-      //     expect(submit).toEqual(true);
-      // });
+
+      it('TC-011 Story type dropdown is present', () => {
+          cy.get(sel.storyType).should('be.visible');
+      });
+
+      it('TC-012 Image field label is present', () => {
+          cy.get(sel.imageLabel).should('be.visible');
+      });
+
+      it('TC-013 Image field is present', () => {
+          cy.get(sel.image).should('be.visible');
+      });
+
+      it('TC-014 Submit button is present', () => {
+          cy.get(sel.submit).should('be.visible');
+      });
 
   });
 
