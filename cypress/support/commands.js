6451b3e4bae8409ce430e53b27937e3109b81110
Sergei Svistelnikov <sergei.svistelnikov@outlook.com>
2021-04-19 11:16:21 -0700

    added some files

---
+++ cypress/support/commands.js
@@ -0,0 +1,33 @@
+import sel from "../fixtures/selectors";
+
+Cypress.Commands.add('login', (username, password) =>{
+    cy.get(sel.username).type(username);
+    cy.get(sel.password).type(password);
+});
+
+
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
